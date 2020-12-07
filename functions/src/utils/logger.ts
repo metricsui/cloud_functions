e7b63779160f1e59f1897afb136776f334343adb
Albertus Angga <albertusangga.98@gmail.com>
2020-12-07 18:33:06 +0700

    Fix blinking bug & Dashboard button bug

--- functions/src/utils/logger.ts
+++ functions/src/utils/logger.ts
@@ -1,13 +1,37 @@
 import * as functions from 'firebase-functions'
+import * as axios from 'axios'
+import * as util from 'util'
 
-// TODO: Push error/warn to alert system?
+function entryFromArgs(severity, args) {
+  let entry = {}
+  const lastArg = args[args.length - 1]
+  if (lastArg && typeof lastArg === 'object' && lastArg.constructor == Object) {
+    entry = args.pop()
+  }
+  return Object.assign({}, entry, {
+    severity,
+    // mimic `console.*` behavior, see https://nodejs.org/api/console.html#console_console_log_data_args
+    message: util.format.apply(null, args),
+  })
+}
+
 export class Logger {
-  static error(...args: any[]) {
+  static error(...args: any[]): void {
     functions.logger.error(...args)
+    setImmediate(async () => {
+      await axios.default.post(functions.config().discord.error_webhook_url, {
+        content: entryFromArgs('ERROR', args),
+      })
+    })
   }
 
-  static warn(...args: any[]) {
+  static warn(...args: any[]): void {
     functions.logger.warn(...args)
+    setImmediate(async () => {
+      await axios.default.post(functions.config().discord.error_webhook_url, {
+        content: entryFromArgs('INFO', args),
+      })
+    })
   }
 
   static info(...args: any[]) {
